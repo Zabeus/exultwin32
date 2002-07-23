e09908a94aa03b6d408075bbca9ed2bee3a16170
wjpalenstijn <wjpalenstijn@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2002-07-23 17:47:36 +0000

    allow # in URL

--- web/trunk/forum/include/read_functions.php
+++ web/trunk/forum/include/read_functions.php
@@ -22,7 +22,7 @@
         if($ForumAllowHTML==1){
             // replace url/link items
             $body=preg_replace("/\[img\]((http|https|ftp):\/\/[a-z0-9;\/\?:@=\&\$\-_\.\+!*'\(\),~]+?)\[\/img\]/i", "<img src=\"$1\" />", $body);
-            $body=preg_replace("/\[url\]((http|https|ftp|mailto):\/\/[a-z0-9;\/\?:@=\&\$\-_\.\+!*'\(\),~]+?)\[\/url\]/i", "<a href=\"$1\">$1</a>", $body);
+            $body=preg_replace("/\[url\]((http|https|ftp|mailto):\/\/[a-z0-9;\/\?:@=\&\$\-_\.\+#!*'\(\),~]+?)\[\/url\]/i", "<a href=\"$1\">$1</a>", $body);
             $body=preg_replace("/\[url=((http|https|ftp|mailto):\/\/[a-z0-9;\/\?:@=\&\$\-_\.\+!*'\(\),~]+?)\](.+?)\[\/url\]/i", "<a href=\"$1\">$3</a>", $body);
             $body=preg_replace("/\[email\]([a-z0-9\-_\.\+]+@[a-z0-9\-]+\.[a-z0-9\-\.]+?)\[\/email\]/i", "<a href=\"mailto:$1\">$1</a>", $body);
 
