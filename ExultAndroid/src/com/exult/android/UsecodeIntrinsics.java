0d1ace1dee52830e57ce15788b151578e71a1d10
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2010-11-30 08:17:34 +0000

    Started writing intrinsics

--- ExultAndroid/src/com/exult/android/UsecodeIntrinsics.java
+++ ExultAndroid/src/com/exult/android/UsecodeIntrinsics.java
@@ -8,16 +8,22 @@ public class UsecodeIntrinsics extends GameSingletons {
 	/*
 	 * The intrinsics:
 	 */
-	private UsecodeValue getRandom(UsecodeValue p0) {
+	private final UsecodeValue getRandom(UsecodeValue p0) {
 		int range = p0.getIntValue();
 		if (range == 0)
 			return UsecodeValue.getZero();
 		return new UsecodeValue.IntValue(1 + (EUtil.rand() % range));
 	}
-	private UsecodeValue getItemShape(UsecodeValue p0) {
+	private final UsecodeValue getItemShape(UsecodeValue p0) {
 		GameObject obj = getItem(p0);
 		return obj == null ? UsecodeValue.getZero() :
 			new UsecodeValue.IntValue(obj.getShapeReal());
+	}
+	private final UsecodeValue getItemFrame(UsecodeValue p0) {
+		GameObject obj = getItem(p0);
+		return obj == null ? UsecodeValue.getZero() :
+			// Don't count rotated frames.
+			new UsecodeValue.IntValue(obj.getFrameNum()&31);
 	}
 	
 	//	For BlackGate.
@@ -27,6 +33,8 @@ public class UsecodeIntrinsics extends GameSingletons {
 			return getRandom(parms[0]);
 		case 0x11:
 			return getItemShape(parms[0]);
+		case 0x12:
+			return getItemFrame(parms[0]);
 		default:
 			System.out.println("UNHANDLED intrinsic # " + id);
 			return UsecodeValue.getZero();
