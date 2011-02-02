8f7e0ca76f1bf1d3d1d9d61cfc77e5f71868157f
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2011-02-02 03:09:27 +0000

    Writing text parsers.

--- ExultAndroid/src/com/exult/android/DataUtils.java
+++ ExultAndroid/src/com/exult/android/DataUtils.java
@@ -1,7 +1,9 @@
 package com.exult.android;
 import java.io.DataInputStream;
 import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Vector;
 
@@ -15,6 +17,167 @@ public final class DataUtils {
 			cnt = EUtil.Read2(in);	// Exult extension.
 		return cnt;
 	}
+	/*
+	 *	Reads text data file and parses it according to passed
+	 *	parser functions.
+	 */
+	public static void readTextDataFile
+		(
+		String fname,			// Name of file to read, sans extension
+		BaseReader parsers[],			// What to use to parse data.
+		String sections[],		// The names of the sections
+		int game,
+		int resource
+		) throws IOException {
+		int numsections = sections.length;
+		int static_version = 1;
+		int patch_version = 1;
+		Vector<Vector<byte[]>> static_strings = new Vector<Vector<byte[]>>();
+		Vector<Vector<byte[]>> patch_strings = new Vector<Vector<byte[]>>();
+		if (game == EConst.BLACK_GATE || game == EConst.SERPENT_ISLE) {
+			boolean bg = game == EConst.BLACK_GATE;
+			String flexfile = bg ? EFile.EXULT_BG_FLX : EFile.EXULT_SI_FLX;
+			EFile txtobj = EFileManager.instanceOf().getFileObject(flexfile);
+			byte txt[] = txtobj.retrieve(resource);
+			InputStream ds = new ByteArrayInputStream(txt);
+			static_version = readTextMsgFileSections(ds,
+					static_strings, sections);
+		} /* +++++++++FINISH
+		else
+			{
+			try
+				{
+				snprintf(buf, 50, "<STATIC>/%s.txt", fname);
+				std::ifstream in;
+				U7open(in, buf, false);
+				static_version = readTextMsgFileSections(in,
+						static_strings, sections, numsections);
+				in.close();
+				}
+			catch (std::exception &e)
+				{
+				if (!editing)
+					throw e;
+				static_strings.resize(numsections);
+				}
+		} */
+		patch_strings.setSize(numsections);
+		String buf = String.format("<PATCH>/%1$s.txt", fname);
+		if (EUtil.U7exists(buf) != null) {
+			InputStream in = EUtil.U7openStream(buf);
+			patch_version = readTextMsgFileSections(in, patch_strings, sections);
+			in.close();
+		}
+		for (int i = 0; i < numsections; i++) {
+			parsers[i].parse(static_strings.elementAt(i), static_version, false, game);
+			parsers[i].parse(patch_strings.elementAt(i), patch_version, true, game);
+		}
+		static_strings.clear();
+		patch_strings.clear();
+	}
+	public static int readTextMsgFileSections
+		(
+		InputStream in,
+		Vector<Vector<byte[]> > strings,	// Strings returned here
+		String sections[]			// Section names
+		) throws IOException {
+		int numsections = sections.length;
+		strings.setSize(numsections);
+		int version = 1;
+
+		Vector<byte[]> versioninfo;
+		in.mark(1000000);
+		/* ++++++++++FINISH
+		// Read version.
+		final String versionstr = "version";
+		if (Search_text_msg_section(in, versionstr) && 
+				readTextMsgFile(in, versioninfo, versionstr) != -1) {
+			version = static_cast<int>(strtol(versioninfo[0], 0, 0));
+		}
+		*/
+		for (int i = 0; i < numsections; i++) {
+			in.reset();
+			//+++++FINISH if (!Search_text_msg_section(in, sections[i]))
+			//++++++	continue;
+			strings.setElementAt(readTextMsgFile(in, sections[i]), i);
+		}
+	return version;
+	}
+	/*
+	 *	Read in text, where each line is of the form "nnn:sssss", where nnn is
+	 *	to be the Flex entry #, and anything after the ':' is the string to
+	 *	store.  
+	 *	NOTES:	Entry #'s may be skipped, and may be given in hex (0xnnn)
+	 *			or decimal.
+	 *		Max. text length is 1024.
+	 *		A line beginning with a '#' is a comment.
+	 *		A 'section' can be marked:
+	 *			%%section shapes
+	 *				....
+	 *			%%endsection
+	 *	Output:	# of first message (i.e., lowest-numbered msg), or -1 if
+	 *		error.
+	 */
+	private static Vector<byte[]> readTextMsgFile(InputStream instream, String section)
+														throws IOException {
+		Vector<byte[]> strings = new Vector<byte[]>(1000);
+		DataInputStream textin = new DataInputStream(instream);
+        BufferedReader in = new BufferedReader(new InputStreamReader(textin));
+		int linenum = 0;
+		final int NONEFOUND = 0xffffffff;
+		int first = NONEFOUND;// Index of first one found.
+		int next_index = 0;// For auto-indexing of lines
+		String s;
+		while ((s = in.readLine()) != null) {
+			++linenum;
+			if (s.length() == 0)
+				continue;	// Empty line.
+			if (section != null) {
+				if (!s.startsWith("%%section", 8))
+					continue;
+				int ind, strlen = s.length();
+				for (ind = 9; ind < strlen && Character.isSpace(s.charAt(ind)); ++ind)
+					;
+				if (s.startsWith(section, ind)) {	// Found the section.
+					section = null;
+					continue;
+				}
+				System.out.println("Line #" + linenum + 
+					" has the wrong section name " + " != " 
+										+ section);
+				return null;
+			}
+			if (s.startsWith("%%endsection"))
+				break;
+			int ind, index, endptr;
+
+			if (s.charAt(0) == ':') {	// Auto-index lines missing an index.
+				index = next_index++;
+				ind = 0;
+			} else {	// Get line# in decimal, hex, or oct.
+				int strlen = s.length();
+				for (ind = 0; ind < strlen && Character.isDigit(s.charAt(ind)); ++ind)
+					;
+				if (ind == 0) {
+					System.out.println("Line " + linenum + " doesn't start with a number");
+					return null;
+				}
+				index = Integer.parseInt(s.substring(0, ind));
+				if (s.charAt(ind) != ':') {
+					System.out.println("Missing ':' in line " + linenum + 
+						".  Ignoring line");
+					continue;
+				}
+			}
+			if (index >= strings.size())
+				strings.setSize(index + 1);
+			strings.setElementAt(s.substring(ind + 1).getBytes(), index);
+			if (index < first)
+				first = index;
+		}
+		return strings;
+	}
+
 	/*
 	 *	Generic base data-agnostic reader class.
 	 */
