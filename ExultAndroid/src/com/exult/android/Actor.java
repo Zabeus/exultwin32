4088d92c455b47c2d4886aab1d63b5d7b49bf3fd
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2011-02-15 21:18:04 +0000

    Spellbook works.

--- ExultAndroid/src/com/exult/android/Actor.java
+++ ExultAndroid/src/com/exult/android/Actor.java
@@ -2709,7 +2709,8 @@ public abstract class Actor extends ContainerGameObject implements TimeSensitive
 			final int xoffs[] = {-1, 1, -2, 2, -3, 3, -4, 4, -5, 5},
 				   	   yoffs[] = {1, -1, 2, -2, 3, -3, 4, -4, 5, -5};
 			if (partyId < 0 || partyId >= xoffs.length) {
-				System.out.println("Actor.follow: partyId = " + partyId);
+				System.out.println("Actor.follow: partyId = " + partyId +
+						", Npc # = " + npcNum);
 				return;
 			}
 			goal.tx += xoffs[partyId] + 1 - EUtil.rand()%3;
