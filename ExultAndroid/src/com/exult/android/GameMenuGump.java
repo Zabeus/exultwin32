7f5abb2c46e06ba26b615049407531a8bd65c402
jsf <jsf@67c084eb-e78b-48f3-b8e7-bfa43f95ef20>
2011-07-27 01:43:52 +0000

    Highlight menu choices.  'Journey Onwards' implemented.

--- ExultAndroid/src/com/exult/android/GameMenuGump.java
+++ ExultAndroid/src/com/exult/android/GameMenuGump.java
@@ -7,6 +7,7 @@ import com.exult.android.Gump.Modal;
  */
 public class GameMenuGump extends Modal {
 	private static final int menuChoices[] = { 0x04, 0x05, 0x08, 0x06, 0x11, 0x12, 0x07 };
+	private GumpWidget.Button selected;
 	
 	public GameMenuGump(ShapeFrame s) {
 		super(s);
@@ -19,29 +20,75 @@ public class GameMenuGump extends Modal {
 			ShapeFrame onShape = menuShapes.getShape(menuChoices[i], 1),
 					  offShape = menuShapes.getShape(menuChoices[i], 0);
 			int width = offShape.getWidth();
-			MenuItem entry = new MenuItem(this, onShape, offShape, centerx - width/2, menuy + offset);
+			MenuItem entry = new MenuItem(this, i, onShape, offShape, centerx - width/2, menuy + offset);
 			addElem(entry);	
 			offset += offShape.getYBelow() + 3;
 		}
 	}
-	// Handle events:
-	public void onUp(int mx, int my) {
+	@Override
+	public void close() {
 		audio.stopMusic();
 		gwin.getPal().fadeOut(EConst.c_fade_out_time);
-		close();//++++++TESTING
+		super.close();
+	}
+	// Handle events:
+	@Override
+	public void onMotion(int mx, int my) {
+		GumpWidget.Button item = onButton(mx, my);
+		if (item != selected) {
+			if (selected != null) {
+				selected.setPushed(false);
+				selected.paint();
+			}
+			if (item != null) {
+				item.setPushed(true);
+				item.paint();
+			}
+			selected = item;
+			gwin.setPainted();
+		}
+	}
+	@Override
+	public void onUp(int mx, int my) {
+		GumpWidget.Button item = onButton(mx, my);
+		if (item != null)
+			item.activate(true);
 	}
 	public void keyDown(int chr) // Key pressed
 		{  }
 	public void textInput(int chr, int unicode) // Character typed (unicode)
 		{ }
+	public void handleChoice(int id) {
+		switch (id) {
+		case 0:	// Intro
+			break;
+		case 1: // New Game
+			break;
+		case 2:	// Journey Onwards
+			close(); break;
+		case 3:	// Credits
+			break;
+		case 4:	// Quotes
+			break;
+		case 5:	// End game
+			break;
+		case 6:	// Return to Exult menu
+			break;
+		}
+	}
+	
 	public static class MenuItem extends GumpWidget.Button {
-		public MenuItem(Gump par, ShapeFrame onShape, ShapeFrame offShape, int px, int py) {
+		int id;
+		public MenuItem(Gump par, int i, ShapeFrame onShape, ShapeFrame offShape, int px, int py) {
 			super(par, onShape, offShape, px, py);
+			id = i;
 		}
 		// What to do when 'clicked':
 		@Override
 		public boolean activate(boolean button) {
-			//++++TODO
+			if (!button) 
+				return false;
+			((GameMenuGump)parent).handleChoice(id);
 			return true;
 		}
 	}
